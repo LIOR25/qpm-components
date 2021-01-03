83a437e1f350857a625663d10f665caed42f9997
Lior <lior2512@gmail.com>
2021-01-03 09:48:40 +0200

    reusable list component

--- src/app/qp-tab/qp-tab.component.ts
+++ src/app/qp-tab/qp-tab.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 
 @Component({
@@ -6,6 +6,14 @@ import { Component } from "@angular/core";
   templateUrl: './qp-tab.component.html',
   styleUrls: ['./qp-tab.component.scss']
 })
-export class QpTabComponent {
+export class QpTabComponent implements OnInit {
+  qpArr;
 
+  constructor() { }
+
+  ngOnInit() {
+    this.qpArr = [{ name: 'QP שם', name2: '14' }, { name: 'מזהה', name2: '106' }, { name: 'סוג', name2: ' שיקום שוחות' },
+    { name: ' נוצר ב ', name2: '20200804_150540' }, { name: ' מספר אי התאמות ', name2: '15' }
+    ]
+  }
 }
