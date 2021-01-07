93d4561abbcb21b007e6c1f83f62646722714c7b
Lior <lior2512@gmail.com>
2021-01-07 11:05:34 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { CarouselComponent } from './components/carousel/carousel.component';
-import { ListComponent } from './components/list/list.component';
-import { List2Component } from './components/list2/list2.component';
-import { PopupComponent } from './components/popup/popup.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { QpTabComponent } from './qp-tab/qp-tab.component';
 
@@ -11,11 +7,7 @@ import { QpTabComponent } from './qp-tab/qp-tab.component';
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomePageComponent },
-  { path: 'list', component: ListComponent },
-  { path: 'list2', component: List2Component },
-  { path: 'qptab', component: QpTabComponent },
-  { path: 'popup', component: PopupComponent },
-  { path: 'carousel', component: CarouselComponent }
+  { path: 'tabs', component: QpTabComponent },
 ];
 
 @NgModule({
